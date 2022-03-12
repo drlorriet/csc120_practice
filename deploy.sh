31bc5066a935debb94b5060885f8beee2766b319
lorrieteaching <67921919+lorrieteaching@users.noreply.github.com>
2022-03-12 15:12:29 -0500

    add directory layer

--- deploy.sh
+++
@@ -1,5 +0,0 @@
-touch ./build/MyAssignments/.nojekyll
-git add build/ -f
-git add ./build/MyAssignments/.nojekyll -f
-git commit -m "Added the current build and .nojekyll file as part of deployment"
-git subtree push --prefix build/MyAssignments origin gh-pages
